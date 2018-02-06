31f29383ce3d43a953026aefb01f9e22193ab65e
Hayley Newton <Newton.H@cambridgeenglish.org>
2018-02-06 17:01:33 +0000

    fixed nav and some structure stuff

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+window.addEventListener('scroll', () => {
+
+    const windowHeight = window.innerHeight - 80;
+
+    if(window.scrollY >= windowHeight) {
+        document.querySelector('nav').classList = 'fixed-header';
+    } else {
+        document.querySelector('nav').classList = '';
+    }
+});
