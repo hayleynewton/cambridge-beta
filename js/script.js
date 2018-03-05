2a3a1425e707e743a9ab0d73afee1d775694d788
Hayley Newton <Newton.H@cambridgeenglish.org>
2018-03-05 16:06:21 +0000

    Merge conflicts resolved

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
-(function() {
-
     // sticky navigation bar
     window.addEventListener('scroll', () => {
 
         const windowHeight = window.innerHeight - 80;
     
         if(window.scrollY >= windowHeight) {
+
             document.querySelector('nav').classList = 'fixed-header';
         } else {
             document.querySelector('nav').classList = '';
         }
     });
 
-})
