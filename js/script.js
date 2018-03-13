c0d6585474abb6f44739297db6c779bb9a1c6afa
Hayley Newton <Newton.H@cambridgeenglish.org>
2018-03-13 13:40:48 +0000

    Auto scroller function - commented out.

--- js/script.js
+++ js/script.js
@@ -11,5 +11,25 @@ function scrollListener() {
     }
 }
 
+// auto scroller
+// function autoScroller() {
+
+//     const contentDiv = document.querySelector('ul[aria-label="featured"]');
+//     const features = contentDiv.querySelectorAll('li');
+//     const currentPage = document.querySelector('.home');
+//     const active = true;
+//     const counter = 1;
+
+//     while(currentPage) {
+//         setInterval(() => {
+//             if(counter <= features.length) {
+//                 //contentDiv.style.left = "-100%";
+//             }
+//         }, 5000);
+//     }
+// }
+
 window.addEventListener('scroll', scrollListener);
 window.addEventListener('touchmove', scrollListener);
+
+
